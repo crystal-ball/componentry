df5544aea40eedd366919393db4f8458ddd5ad78
Dan Hedgecock <hedgecock.d@gmail.com>
2017-09-01 19:30:33 -0700

    Test eslint prettier in test env mode with prepush

--- src/index.jsx
+++ src/index.jsx
@@ -16,8 +16,4 @@ const render = Component => {
 
 render(App)
 
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    render(App)
-  })
-}
+if (module.hot) { module.hot.accept('./App', () => {render(App)})}
