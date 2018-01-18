5585db33c72865e8e1dfce3436e044b2b4083643
Dan Hedgecock <hedgecock.d@gmail.com>
2018-01-18 08:14:26 -0800

    refactor(components): modal component layout updated to better align with Bootstrap
    The modal component layout and event handling has been updated to align with Bootstrap modals,
    improvements include reduction of custom library styles
    
    BREAKING CHANGE:
    React Fragments used for the first time, library now requires ^16.2

--- lib/Modal/Modal.jsx
+++ lib/Modal/Modal.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import type { Node } from 'react'
 import { object, shape, string } from 'prop-types'
 import classNames from 'classnames'
@@ -19,19 +19,16 @@ type Props = {
   visible?: boolean,
 }
 
-// /**
-//  * TODO:
-//  * - Close button auto-wiring of aria-label=close && deactivate
-//  * - Close on escape** - All toggle items!
-//  * - Auto focus on open
-//  * - Animated open/close
-//  * - Docs on passing flex align-items-center/start to header for close icon alignment
-//  * - Add close button when `ariaTitle is used
-//  *
-//  * ## Notes:
-//  * See MDN [Using the dialog role](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_dialog_role)
-//  */
-
+/**
+ * TODO:
+ * - Close button auto-wiring of aria-label=close && deactivate
+ * - Auto focus on open
+ * - Docs on passing flex align-items-center/start to header for close icon alignment
+ * - Add close button when `ariaTitle is used
+ *
+ * ## Notes:
+ * See MDN [Using the dialog role](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_dialog_role)
+ */
 class Modal extends Component<Props> {
   static Header = elementFactory({ classes: 'modal-header', name: 'ModalHeader' })
   static Body = elementFactory({ classes: 'modal-body', name: 'ModalBody' })
@@ -51,12 +48,42 @@ class Modal extends Component<Props> {
    */
   guid = nanoid()
 
+  // Hooks
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Handle adding/removing the `modal-open` class to the body when the modal is
+   * opening and closing. This allows long modals to scroll within the `.modal`
+   * container.
+   *
+   * 🤔 This could move to the activate/deactivate hooks inside one  of the active
+   * component factories which would allow us to more explicitly know when the modal
+   * is opening/closing but also makes it less apparent where that change is coming
+   * from...
+   */
   componentWillReceiveProps(nextProps) {
-    if (!this.props.active && nextProps.active) {
-      document.body.classList.add('modal-open')
-    }
-    if (this.props.active && !nextProps.active) {
-      document.body.classList.remove('modal-open')
+    const { active } = this.props
+    if (!active && nextProps.active) document.body.classList.add('modal-open')
+    if (active && !nextProps.active) document.body.classList.remove('modal-open')
+  }
+
+  // Methods
+  // ---------------------------------------------------------------------------
+
+  /**
+   * The `.modal` container has a higher z-index than the `.modal-backdrop`, and
+   * receives any clicks targeted at the backdrop. In order to close the modal on
+   * any click of the backdrop, this handler checks if any click within the modal is
+   * on the `.modal`, if the click is inside of the modal *CONTENT*, then the target
+   * will be that content. If the target is outside of the modal content, the target
+   * will be the `.modal` container and we know the user is clicking outside the
+   * modal to close it.
+   */
+  handleModalClick = evt => {
+    const { target } = evt
+
+    if (target.classList && target.classList.contains('modal')) {
+      this.props.deactivate(evt)
     }
   }
 
@@ -65,44 +92,53 @@ class Modal extends Component<Props> {
   render() {
     const THEME = this.context.THEME || {}
     const componentCtx = THEME.Modal || {}
-    const { active, ariaTitle, children, deactivate, size, visible }: Props = {
+    const { active, ariaTitle, children, size, visible }: Props = {
       ...componentCtx,
       ...this.props,
     }
 
     return (
-      <div
-        aria-hidden={active ? 'false' : 'true'}
-        aria-labelledby={`${this.guid}`}
-        className={classNames('modal', 'fade', { show: visible })}
-        role="dialog"
-        tabIndex="-1"
-      >
-        {/* See SCSS file for explanation on backdrop markdown order/position */}
-
+      <Fragment>
+        {/* ℹ️ Bootstrap modal layout uses the .modal container in such a way that
+        it receives all click events outside the modal content. This requires adding
+        the modal click handler. This handler is _just_ for mouse users, so we are
+        disabling the aria linters for adding mouse events to a dialog. Keyboard
+        users can close the dialog with an esc keypress */}
+        {/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */}
+        {/* eslint-disable jsx-a11y/click-events-have-key-events */}
         <div
-          className={classNames('modal-dialog', { [`modal-${size}`]: size })}
-          role="document"
+          aria-hidden={active ? 'false' : 'true'}
+          aria-labelledby={`${this.guid}`}
+          className={classNames('modal', 'fade', { show: visible })}
+          role="dialog"
+          tabIndex="-1"
+          onClick={this.handleModalClick}
         >
-          <div className="modal-content">
-            {/* A++ Accessibility title for modals without visual title */}
-            {ariaTitle && (
-              <div id={this.guid} className="sr-only">
-                {ariaTitle}
-                {/* TODO: Include close button for aria title modals */}
-              </div>
-            )}
-            {children}
+          <div
+            className={classNames('modal-dialog', {
+              'modal-sm': size === 'small',
+              'modal-lg': size === 'large',
+            })}
+            role="document"
+          >
+            <div className="modal-content">
+              {/* A++ Accessibility title for modals without visual title */}
+              {ariaTitle && (
+                <div id={this.guid} className="sr-only">
+                  {ariaTitle}
+                  {/* TODO: Include close button for aria title modals */}
+                </div>
+              )}
+              {children}
+            </div>
           </div>
         </div>
         <div
           aria-hidden={active ? 'false' : 'true'}
           className={classNames('modal-backdrop', 'fade', { show: visible })}
-          onClick={deactivate}
-          onKeyPress={deactivate}
           role="presentation"
         />
-      </div>
+      </Fragment>
     )
   }
 }
