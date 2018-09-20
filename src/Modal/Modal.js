35d196c06164c9ae475ddeb3f63b8b69c53f97e8
Dan Hedgecock <hedgecock.d@gmail.com>
2018-09-20 08:56:43 -0700

    refactor(components): handle modal body click with stopPropogation

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -95,26 +95,6 @@ class Modal extends Component<Props> {
     window.document.body.classList.remove('no-scroll')
   }
 
-  // Methods
-  // ---------------------------------------------------------------------------
-
-  /**
-   * The `.modal` container has a higher z-index than the `.modal-backdrop`, and
-   * receives any clicks targeted at the backdrop. In order to close the modal on
-   * any click of the backdrop, this handler checks if any click within the modal is
-   * on the `.modal`, if the click is inside of the modal *CONTENT*, then the target
-   * will be that content. If the target is outside of the modal content, the target
-   * will be the `.modal` container and we know the user is clicking outside the
-   * modal to close it.
-   */
-  handleModalClick = evt => {
-    const { target } = evt
-
-    if (target.classList && target.classList.contains('modal')) {
-      this.props.deactivate(evt)
-    }
-  }
-
   // Render
   // ---------------------------------------------------------------------------
   render() {
@@ -123,27 +103,25 @@ class Modal extends Component<Props> {
     return (
       <ModalContext.Provider value={{ deactivate, guid: this.guid }}>
         <Fragment>
-          {/* ℹ️ Bootstrap modal layout uses the .modal container in such a way that
-        it receives all click events outside the modal content. This requires adding
-        the modal click handler. This handler is _just_ for mouse users, so we are
-        disabling the aria linters for adding mouse events to a dialog. Keyboard
-        users can close the dialog with an esc keypress */}
-          {/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */}
-          {/* eslint-disable jsx-a11y/click-events-have-key-events */}
           <div
+            onClick={deactivate}
+            className={classNames('modal', 'fade', { show: visible })}
             aria-hidden={active ? 'false' : 'true'}
             aria-labelledby={`${this.guid}`}
-            className={classNames('modal', 'fade', { show: visible })}
-            role="dialog"
+            role="presentation"
             tabIndex="-1"
-            onClick={this.handleModalClick}
           >
+            {/* ℹ️ Stop propogation of clicks inside modal or they will trigger
+            the modal background deactivate handler */}
+            {/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */}
+            {/* eslint-disable jsx-a11y/click-events-have-key-events */}
             <div
+              onClick={evt => evt.stopPropagation()}
               className={classNames('modal-dialog', {
                 'modal-sm': size === 'small',
                 'modal-lg': size === 'large',
               })}
-              role="document"
+              role="dialog"
             >
               <div className="modal-content">{children}</div>
             </div>
