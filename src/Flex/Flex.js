c3327835b59669deb61cb9ab51258feb0a4e7340
Dan Hedgecock <hedgecock.d@gmail.com>
2018-09-23 07:14:59 -0700

    refactor: use default props with Flex component

--- src/Flex/Flex.js
+++ src/Flex/Flex.js
@@ -4,22 +4,18 @@ import withTheme from '../withTheme'
 
 type Props = {
   /** Specify the flex direction of the container */
-  direction?: '' | 'column' | 'column-reverse' | 'row' | 'row-reverse',
+  direction: '' | 'column' | 'column-reverse' | 'row' | 'row-reverse',
   /** Toggles between display flex and display inline-flex */
-  inline?: boolean,
+  inline: boolean,
   /** Adds flexbox align-items atomic classes */
-  align?: '' | 'start' | 'center' | 'end' | 'baseline' | 'stretch',
+  align: '' | 'start' | 'center' | 'end' | 'baseline' | 'stretch',
   /** Adds flexbox justify-content atomic classes */
-  justify?: '' | 'start' | 'center' | 'end' | 'around' | 'between',
+  justify: '' | 'start' | 'center' | 'end' | 'around' | 'between',
+  /** Sets a flex-wrap utility class */
+  wrap: '' | 'wrap' | 'wrap-reverse' | 'nowrap',
 }
 
-const Flex = ({
-  align = '',
-  direction = '',
-  inline = false,
-  justify = '',
-  ...rest
-}: Props) =>
+const Flex = ({ align, direction, inline, justify, wrap, ...rest }: Props) =>
   componentryElem({
     classes: {
       'd-flex': !inline,
@@ -29,9 +25,17 @@ const Flex = ({
       'flex-row': direction === 'row',
       'flex-row-reverse': direction === 'row-reverse',
       [`align-items-${align}`]: align,
+      [`flex-${wrap}`]: wrap,
       [`justify-content-${justify}`]: justify,
     },
     ...rest,
   })
+Flex.defaultProps = {
+  align: '',
+  direction: '',
+  inline: false,
+  justify: '',
+  wrap: '',
+}
 
 export default withTheme('Flex', Flex)
