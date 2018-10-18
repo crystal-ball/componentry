a04f0033ec8719bcb8e48e20225e48f97391f2e7
Dan Hedgecock <hedgecock.d@gmail.com>
2018-10-18 12:02:49 -0700

    fixes

--- demo/components/App/AnchorLink.js
+++ demo/components/App/AnchorLink.js
@@ -1,25 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react'
 import { Link } from 'react-router-dom'
-
-type Props = {
-  /** Component children */
-  children?: Node,
-  /** External url */
-  href?: string,
-}
 
 /**
  * The react router Link does *NOT* handle external links, which is irritating.
  * This component checks for an href, and renders an `a` element when found,
  * otherwise it renders a Link for the internal link.
  */
-const AnchorLink = ({ children, to, ...rest }: Props) =>
-  to ? (
-    <Link to={to} {...rest}>
-      {children}
-    </Link>
-  ) : (
-    <a {...rest}>{children}</a>
-  )
-
+const AnchorLink = props => (props.to ? <Link {...props} /> : <a {...props} />)
 export default AnchorLink
--- src/Type/Type.js
+++ src/Type/Type.js
@@ -36,26 +36,21 @@ const makeType = as => ({
   size,
   textAlign,
   ...rest
-}: Props) => {
-  const classes = {
-    'font-italic': italic,
-    'text-uppercase': uppercase,
-    'text-monospace': monospace,
-    [`font-weight-${fontWeight}`]: fontWeight,
-    [`text-${color}`]: color,
-    [`text-${size}`]: size,
-    [`text-${textAlign}`]: textAlign,
-  }
-
-  if (as === 'a') classes.disabled = !rest.href
-
-  return elem({
+}: Props) =>
+  elem({
     defaultAs: as,
     style: { fontSize, letterSpacing, lineHeight },
-    classes,
+    classes: {
+      'font-italic': italic,
+      'text-uppercase': uppercase,
+      'text-monospace': monospace,
+      [`font-weight-${fontWeight}`]: fontWeight,
+      [`text-${color}`]: color,
+      [`text-${size}`]: size,
+      [`text-${textAlign}`]: textAlign,
+    },
     ...rest,
   })
-}
 
 const Anchor = withTheme('Anchor', makeType('a'))
 const Header = withTheme('Header', makeType('h1'))
