2720954f74e7a3cd923779f06af7fe2f886fa3a8
Dan Hedgecock <hedgecock.d@gmail.com>
2018-10-19 13:27:32 -0700

    feat: add color utility className to Icon

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,21 +1,15 @@
-// @flow
 import React from 'react'
 import elem from '../elem-factory'
 import withTheme from '../withTheme'
 
-type Props = {
-  font?: boolean,
-  id: string,
-}
-
 /**
  * ⚠️ Requires SVGs are inlined into document somewhere
  */
-export default withTheme('Icon', ({ id, font = true, ...rest }: Props) =>
+export default withTheme('Icon', ({ color, id, font = true, ...rest }) =>
   elem({
     defaultAs: 'svg',
     role: 'img',
-    classes: [`icon icon-${id}`, { font }],
+    classes: [`icon icon-${id}`, { font, [`text-${color}`]: color }],
     children: <use href={`#${id}`} xlinkHref={`#${id}`} />,
     ...rest,
   }),
