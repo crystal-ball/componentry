efc47b800fda5543361c0f4105b66e40142ebb88
Dan Hedgecock <hedgecock.d@gmail.com>
2019-08-30 19:42:21 -0700

    Test: Fix test coverage metrics and enable notifications again 🎉

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,18 @@
 /* eslint-env node */
 
-const { JEST_NOTIFY, ENABLE_JEST_NOTIFICATIONS } = process.env
+const { ENABLE_JEST_NOTIFICATIONS } = process.env
 
 module.exports = {
   // Provides nice test output of what's being run
   verbose: true,
 
-  // OS notifications of test results in watch mode only
-  // 😢 https://github.com/facebook/jest/issues/8036
-  notify: Boolean(JEST_NOTIFY) && Boolean(ENABLE_JEST_NOTIFICATIONS),
+  // OS notifications of test results is an opt in for funesies
+  notify: Boolean(ENABLE_JEST_NOTIFICATIONS),
 
   // Test coverage can be enforced with a coverageThreshold
   collectCoverage: true,
   coverageReporters: ['text-summary'],
-  collectCoverageFrom: ['src/**/*.js'],
+  collectCoverageFrom: ['src/**/*.js', '!**/*.stories.js'],
 
   // Pre/Post test framework setup configs
   setupFilesAfterEnv: ['<rootDir>/test/jest-extend.js'],
