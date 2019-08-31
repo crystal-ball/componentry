07946a4180a5a1ef860513f4f5da4591aeb1af53
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-08-30 20:12:07 -0700

    Chore: Story and test for Input (#121)

---
+++ src/Input/Input.spec.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Input from './Input'
+import elementTests from '../../test/element-tests'
+
+describe('<Input />', () => {
+  // Basic library element test suite
+  // elementTests(Input) - no wrapper element to test...
+  elementTests(Input.Label)
+  // elementTests(Input.Field) - TODO: cannot test Input because it cannot have children
+  elementTests(Input.Description)
+  elementTests(Input.Error)
+})
+
+// Snapshots
+// ---------------------------------------------------------------------------
+describe('<Input /> snapshots', () => {
+  it('renders correctly', () => {
+    const { container, debug } = render(
+      <div>
+        <Input>
+          <Input.Label>Storybook</Input.Label>
+          <Input.Field />
+          <Input.Description>Really important input!</Input.Description>
+          <Input.Error>Oh no!</Input.Error>
+        </Input>
+      </div>,
+    )
+    debug()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
