a777996ca42c162a487dcf53057f8b5296602c82
Dan Hedgecock <hedgecock.d@gmail.com>
2019-08-30 19:26:53 -0700

    Chore: Drawer story and tests

---
+++ src/Drawer/Drawer.js
@@ -0,0 +1,9 @@
+import activeContainer from '../active-container-factory'
+import activeContent from '../active-content-factory'
+import activeTrigger from '../active-trigger-factory'
+
+const Drawer = activeContainer('drawer')
+export default Drawer
+
+Drawer.Content = activeContent('drawer', { arias: { id: true, hidden: true } })
+Drawer.Trigger = activeTrigger('drawer', { arias: { controls: true, expanded: true } })
