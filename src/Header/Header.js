c28ae0cbd42b71e39680ac21d166d4ffaa7b8026
Dan Hedgecock <hedgecock.d@gmail.com>
2019-09-02 11:43:24 -0700

    New: Scope anchor and header styles to className only ✨

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,5 +2,6 @@ import elem from '../elem-factory'
 import { useTheme } from '../Theme/Theme'
 
 export default function Header(props) {
-  return elem({ as: 'h1', ...useTheme('Header'), ...props })
+  const merged = { as: 'h1', ...useTheme('Header'), ...props }
+  return elem({ componentClassNames: merged.as, ...merged })
 }
--- src/Text/Text.js
+++ src/Text/Text.js
@@ -1,6 +1,11 @@
 import elem from '../elem-factory'
 import { useTheme } from '../Theme/Theme'
 
+/**
+ * Text component
+ * TODO: use `span` prop to render a span?
+ */
 export default function Text(props) {
-  return elem({ as: 'p', ...useTheme('Text'), ...props })
+  const merged = { as: 'p', ...useTheme('Text'), ...props }
+  return elem({ componentClassNames: merged.as, ...merged })
 }
