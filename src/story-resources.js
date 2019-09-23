a1cf7dfe5160731683731653b8eb55ff01ad5827
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-09-23 08:14:51 -0700

    Chore: Move styles into src 💥 (#132)

--- src/story-resources.js
+++ src/story-resources.js
@@ -24,6 +24,32 @@ const components = {
   td: props => createElement('td', props),
 }
 
-export default function ElementsProvider({ children }) {
+export function ElementsProvider({ children }) {
   return <MDXProvider components={components}>{children}</MDXProvider>
 }
+
+export const colors = [
+  'primary',
+  'secondary',
+  'info',
+  'success',
+  'warning',
+  'danger',
+  'background',
+  'foreground',
+]
+
+export const grayscale = [100, 200, 300, 400, 500, 600, 700, 800, 900]
+
+export const sizes = ['sm', 'lg']
+
+export const alignments = [
+  'top',
+  'middle',
+  'bottom',
+  'baseline',
+  'text-top',
+  'text-bottom',
+  'super',
+  'sub',
+]
