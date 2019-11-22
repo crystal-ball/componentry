12c8dc9b903b524d8899e85d2cbc7657c2c71e78
Dan Hedgecock <Hedgecock.D@gmail.com>
2019-11-22 07:05:10 -0800

    Chore: Remove ElementsProvider (#144)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import { addDecorator, addParameters, configure } from '@storybook/react'
 import { themes } from '@storybook/theming'
 import { withKnobs } from '@storybook/addon-knobs'
@@ -20,6 +20,30 @@ addParameters({
       ...themes.light,
       brandTitle: 'Componentry',
       brandImage: null,
+    },
+  },
+})
+
+// Override the Styled Components that the MDX provider uses for standard DOM
+// elements so that we can use Componentry styles classNames.
+addParameters({
+  docs: {
+    components: {
+      div: props => createElement('div', props),
+      span: props => createElement('span', props),
+      h1: props => createElement('h1', props),
+      h2: props => createElement('h2', props),
+      h3: props => createElement('h3', props),
+      h4: props => createElement('h4', props),
+      p: props => createElement('p', props),
+      hr: props => createElement('hr', props),
+      a: props => createElement('a', props),
+      table: props => createElement('table', props),
+      thead: props => createElement('thead', props),
+      tbody: props => createElement('tbody', props),
+      tr: props => createElement('tr', props),
+      th: props => createElement('th', props),
+      td: props => createElement('td', props),
     },
   },
 })
