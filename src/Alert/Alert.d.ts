cca21cd532ac7c8fd41736e2a2c2d093dc957d8c
Dan Hedgecock <hedgecock.d@gmail.com>
2019-11-22 07:27:17 -0800

    Fix: Alert type definition typo ✓

--- src/Alert/Alert.d.ts
+++ src/Alert/Alert.d.ts
@@ -1,12 +1,12 @@
 import * as React from 'react'
 
 interface AlertProps {
-  active?: boolean,
-  ariaTitle?: string,
-  color?: string,
-  deactivate?: Function,
-  dismissible:? boolean,
-  outline:? boolean,
+  active?: boolean
+  ariaTitle?: string
+  color?: string
+  deactivate?: Function
+  dismissible?: boolean
+  outline?: boolean
 }
 
 /** https://crystal-ball.github.io/componentry/components/block */
