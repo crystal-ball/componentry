96d708c8bbbfed4cb7d269d8052cde9e22332e48
Dan Hedgecock <hedgecock.d@gmail.com>
2019-11-26 06:08:12 -0800

    Fix: Wrap media listeners mount in useEffect

--- src/Media/Media.js
+++ src/Media/Media.js
@@ -1,9 +1,41 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 
 /**
  * Media Context
  */
 const MediaCtx = createContext({})
+
+/**
+ * Calculates the state for each breakpoint based on current window width
+ * @param {Array} breakpoints Set of application breakpoints
+ */
+function calcBreakpoints(breakpoints) {
+  const w = window.innerWidth
+
+  return {
+    sm: w < breakpoints[1],
+    md: w >= breakpoints[1] && w < breakpoints[2],
+    lg: w >= breakpoints[2],
+  }
+}
+
+/**
+ * Uses `window.matchMedia` to listen for changes to window media queries and
+ * updates breakpoint status on every change.
+ * @param {Array} breakpoints Set of application breakpoints
+ * @param {func} updateBps Hook state update function
+ */
+function mountListeners(breakpoints, updateBps) {
+  function setBreakpoints() {
+    updateBps(calcBreakpoints(breakpoints))
+  }
+
+  breakpoints.forEach(bp => {
+    // https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Testing_media_queries#Receiving_query_notifications
+    const mq = window.matchMedia(`(min-width: ${bp}px)`)
+    mq.addListener(setBreakpoints)
+  })
+}
 
 /**
  * The Media provider accepts breakpoints and can be used with the `useMedia`
@@ -25,22 +57,13 @@ const MediaCtx = createContext({})
  * orientation, dark mode, etc.
  */
 export default function Media({ children, breakpoints = [0, 768, 1250] }) {
-  const calcBreakpoints = w => ({
-    sm: w < breakpoints[1],
-    md: w >= breakpoints[1] && w < breakpoints[2],
-    lg: w >= breakpoints[2],
-  })
+  const [bps, updateBps] = useState(calcBreakpoints(breakpoints))
 
-  const [bps, updateBps] = useState(calcBreakpoints(window.innerWidth))
-  const setBreakpoints = () => {
-    updateBps(calcBreakpoints(window.innerWidth))
-  }
-
-  breakpoints.forEach(bp => {
-    // https://developer.mozilla.org/en-US/docs/Web/CSS/Media_Queries/Testing_media_queries#Receiving_query_notifications
-    const mq = window.matchMedia(`(min-width: ${bp}px)`)
-    mq.addListener(setBreakpoints)
-  })
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    mountListeners(breakpoints, updateBps)
+  }, [])
+  /* eslint-enable react-hooks/exhaustive-deps */
 
   return <MediaCtx.Provider value={bps}> {children}</MediaCtx.Provider>
 }
