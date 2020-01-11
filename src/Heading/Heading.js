fe6a7d4976e6feef9281d92b79ec6c54660b0e48
Dan Hedgecock <hedgecock.d@gmail.com>
2020-01-11 15:56:22 -0800

    Fix: Heading variants

--- src/Heading/Heading.js
+++ src/Heading/Heading.js
@@ -1,6 +1,19 @@
 import elem from '../elem-factory'
 import { useTheme } from '../Theme/Theme'
 
+const asMap = {
+  'display-1': 'h1',
+  'display-2': 'h1',
+  'heading-1': 'h1',
+  'heading-2': 'h2',
+  'heading-3': 'h3',
+  'heading-4': 'h4',
+  'heading-5': 'h5',
+  'subheading-1': 'h3',
+  'subheading-2': 'h4',
+  'subheading-3': 'h5',
+}
+
 /**
  * Heading element
  * @param {Object} props
@@ -8,14 +21,13 @@ import { useTheme } from '../Theme/Theme'
  */
 export default function Heading(props) {
   const { as, display, variant, ...rest } = {
-    as: 'h1',
-    variant: 'heading',
+    variant: 'heading-1',
     ...useTheme('Heading'),
     ...props,
   }
   return elem({
-    as,
-    elemClassName: [`${variant}-${as.slice(1)}`, { [`display-${display}`]: display }],
+    as: as || asMap[variant],
+    elemClassName: variant,
     ...rest,
   })
 }
