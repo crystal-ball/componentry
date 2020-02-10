92d8bd9b3df5c4e2b1e4f1fef9121e04e6aeb83a
Dan Hedgecock <hedgecock.d@gmail.com>
2020-02-09 21:03:52 -0800

    Update: Consistently use library content and trigger names for Tabs ⚙️

--- src/Tabs/Tabs.js
+++ src/Tabs/Tabs.js
@@ -5,28 +5,33 @@ import { useTheme } from '../Theme/Theme'
 import elem from '../elem-factory'
 import { navClasses } from '../utils/componentry'
 
+/** Tabs component */
 const Tabs = activeContainer('tabs', { name: 'tabs' })
 export default Tabs
 
+/** Tabs content container component */
 Tabs.ContentContainer = function TabsContentContainer(props) {
   return elem({
-    elemClassName: 'tabs-panes-container',
+    elemClassName: 'tabs-content-container',
     ...useTheme('TabsContentContainer'),
     ...props,
   })
 }
 Tabs.ContentContainer.displayName = 'TabsContentContainer'
 
+/** Tabs content component */
 Tabs.Content = activeContent('tab', {
   arias: { hidden: true, role: 'tabpanel' },
-  baseClass: 'tabs-panes-pane', // tab-content-pane
+  baseClass: 'tabs-content',
 })
+Tabs.Content.displayName = 'TabsContent'
 
+/** Tabs nav component */
 Tabs.Nav = function TabsNav(props) {
   return elem({
     as: 'nav',
     role: 'tablist',
-    elemClassName: ['tabs-nav-container', navClasses(props)],
+    elemClassName: ['tabs-nav', navClasses(props)],
     ...useTheme('TabsNav'),
     ...props,
   })
@@ -35,10 +40,12 @@ Tabs.Nav.displayName = 'TabsNav'
 
 // TODO: This should probably be defaultAs a nav item... issues:
 // If making a tab with anchors, these should have class 'nav-link'
+/** Tabs trigger component */
 Tabs.Trigger = activeTrigger('tab', {
   arias: { selected: true, role: 'tab' },
   // TODO: Should this really have default nav-link and nav-item classes??
-  baseClass: 'tabs-nav-tab',
+  baseClass: 'tabs-trigger',
   // Tabs can only activate, they never deactivate when clicked
   action: 'activate',
 })
+Tabs.Trigger.displayName = 'TabsTrigger'
