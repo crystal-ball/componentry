2afe0d413866afa9b9e973ba364f9295c0fc74b3
Dan Hedgecock <Hedgecock.D@gmail.com>
2020-05-12 17:40:57 -0600

    Update: Icon to v3 standards ⚙️ (#218)

--- src/Theme/Theme.js
+++ src/Theme/Theme.js
@@ -9,7 +9,7 @@ const ThemeCtx = createContext({})
 export default function Theme({ children, theme }) {
   return <ThemeCtx.Provider value={theme}>{children}</ThemeCtx.Provider>
 }
-Theme.displayName = 'Theme'
+Theme.displayName = '✨Theme'
 
 /**
  * [Theme hook 📝](https://componentry.design/components/theme)
