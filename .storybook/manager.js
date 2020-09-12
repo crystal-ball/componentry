0af453b116a3aa798f245c799aaedde495cb0dc8
Dan Hedgecock <hedgecock.d@gmail.com>
2020-09-12 09:38:48 -0700

    Chore: Storybook config upgrades ⚙️

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,14 +1,8 @@
 import { create } from '@storybook/theming/create'
 import { addons } from '@storybook/addons'
 
-// Customize dispaly of Storybook to be hecka rad
 addons.setConfig({
-  panelPosition: 'right',
-  // Show addon panel by default on right
-  showPanel: true,
-  // Show all the nav options
-  showRoots: true,
-  // --- Theme
+  // --- Customize theme of Storybook to be hecka rad
   theme: create({
     base: 'dark',
     brandTitle: 'Componentry',
