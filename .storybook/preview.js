cdea2a7a4fb6a65cae273eaadbf7716f5930c07b
Dan Hedgecock <hedgecock.d@gmail.com>
2020-09-27 18:59:06 -0700

    Fix: Storybook configs ⚙️

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,23 +1,14 @@
-import { Meta } from '@storybook/addon-docs/blocks'
-
 import { setupOutlineHandlers } from '../src'
 
 // --- Load Componentry styles ---
 
 import './storybook.scss'
-import { DocsTitle } from './components/DocsTitle'
 
 // --- Setup global decorators --
 
 setupOutlineHandlers()
 
 export const parameters = {
-  docs: {
-    components: {
-      DocsTitle,
-      Meta,
-    },
-  },
   layout: 'centered',
   // --- Options
   options: {
