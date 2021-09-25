01b3e48bb9934c8506b873d9c22fb11f8bf77ea8
Dan Hedgecock <hedgecock.d@gmail.com>
2021-09-25 15:22:43 -0700

    Fix: Get Storybook running again

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,23 @@
 'use strict'
 
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/**/*.stories.@(js|mdx)'],
-  addons: [
-    '@storybook/preset-scss',
-    '@storybook/addon-essentials',
-    '@storybook/addon-links',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-links'],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    })
+
+    // Return the altered config
+    return config
+  },
 }
