ef9047e74986cc650d36b0903a79da95a80659ca
Dan Hedgecock <hedgecock.d@gmail.com>
2021-12-21 10:23:21 -0800

    Theme finalization for Button

--- src/Button/Button.ts
+++ src/Button/Button.ts
@@ -10,12 +10,12 @@ interface ButtonProps extends ComponentBaseProps<'button'> {
   color?: 'primary'
   /** Disables the element, preventing mouse and keyboard events */
   disabled?: boolean
+  /** Toggles full width element layout */
+  fullWidth?: boolean
   /** HTML element href */
   href?: string
   /** Sets the display size */
   size?: 'sm' | 'lg'
-  /** Sets the HTML element to an anchor */
-  to?: string
   /** Display variant */
   variant?: 'primary' | 'secondary'
 }
@@ -30,6 +30,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) =>
     active,
     color,
     disabled,
+    fullWidth,
     size,
     ...merged
   } = {
@@ -37,19 +38,18 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) =>
     ...props,
   }
 
-  // If an href or to is passed, this instance should render an anchor tag
-  const asAnchor = Boolean(merged.href || merged.to)
-
   return element<ButtonProps>('Button', {
-    as: asAnchor ? 'a' : 'button',
     ref,
-    type: asAnchor ? undefined : 'button',
+    // If an href is passed, this instance should render an anchor tag
+    as: merged.href ? 'a' : 'button',
+    type: merged.href ? undefined : 'button',
     disabled,
     componentCx: [
       `button-${variant}`,
       {
         [`button-${size}`]: size,
         [`button-color-${color}`]: color,
+        'w-full': fullWidth,
         active,
         disabled, // We include a disabled class AND pass disabled prop to element for a11y
       },
