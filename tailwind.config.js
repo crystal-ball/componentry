d96e791ede9f3c26fdd7555a6a3b05d05e6e80ff
Dan Hedgecock <hedgecock.d@gmail.com>
2022-01-07 06:57:03 -0800

    Theming updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   content: ['./src/**/*.ts', './src/**/*.mdx'],
   theme: {
     extend: {},
+    spacing: {
+      0: 0,
+      0.5: '0.25rem', // 4px
+      1: '0.5rem', // 8px
+      1.5: '0.75rem', // 12px
+      2: '1rem', // 16px
+      3: '1.5rem', // 24px
+      4: '2rem', // 32px
+      5: '2.5rem', // 40px
+      6: '3rem', // 48px
+    },
   },
   plugins: [],
   corePlugins: {
