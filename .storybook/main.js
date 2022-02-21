629bb4c30d2ed5afb7830eb19198dc8bfa2762c6
Dan Hedgecock <hedgecock.d@gmail.com>
2022-02-21 12:13:37 -0800

    Revert back to /types directory

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,5 +21,5 @@ module.exports = {
     },
     '@geometricpanda/storybook-addon-badges',
   ],
-  stories: ['../src/**/*.stories.@(js|mdx)'],
+  stories: ['../src/**/*.stories.@(js|mdx)', '../docs/**/*.stories.mdx'],
 }
