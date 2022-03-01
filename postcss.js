487695f4956ab7ba2cb016e61d95027ca81e9bd2
Dan Hedgecock <hedgecock.d@gmail.com>
2022-03-01 05:03:02 -0800

    Upgrade package export definitions

--- postcss.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-
-/**
- * ℹ️ This is the PostCSS plugin entry file, it uses the CommonJS compiled
- * assets after publishing.
- */
-module.exports = require('./dist/commonjs/plugin-postcss/plugin').plugin
