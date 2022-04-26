6060237ff72ed89b0027fed79c39d457c026ce9a
Dan Hedgecock <hedgecock.d@gmail.com>
2022-04-26 11:41:43 -0700

    Upgrade: Simpler Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,32 +3,16 @@
 'use strict'
 
 const plugin = require('tailwindcss/plugin')
-const { borderPlugin, createTheme } = require('./dist/commonjs')
+const { borderPlugin, createTheme, tailwindSafelist } = require('./dist/commonjs')
 
 const { height, width, ...themeValues } = createTheme()
 
 module.exports = {
-  // Scan .ts files, all of the required Tailwind classes are included in types
   content: ['./src/**/*.ts', './src/**/*.mdx'],
   corePlugins: {
     preflight: false,
   },
   plugins: [plugin(borderPlugin)],
-  theme: {
-    extend: {},
-    ...themeValues,
-    // Semantic colors
-    textColor: {
-      inverse: '#eff',
-    },
-    backgroundColor: {
-      primary: themeValues.colors.primary,
-    },
-  },
-
-  safelist: [
-    // Semantic colors
-    { pattern: /bg-primary-[\d.]+/ },
-    { pattern: /text-(inverse)/ },
-  ],
+  theme: themeValues,
+  safelist: tailwindSafelist,
 }
