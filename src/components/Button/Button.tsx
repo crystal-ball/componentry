a919ebdabb592b6b11b767c9083980ad35283e86
Dan Hedgecock <Hedgecock.D@gmail.com>
2022-05-01 12:18:03 -0700

    New: Button icons and disabled wrapper (#456)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,23 +1,32 @@
-import { forwardRef } from 'react'
+import clsx from 'clsx'
+import { type ForwardedRef, type ReactElement, type ReactNode, forwardRef } from 'react'
 import { type ComponentBaseProps } from '../../utils/base-types'
 import { element } from '../../utils/element-creator'
 import { type MergePropTypes } from '../../utils/types'
+import { Icon } from '../Icon/Icon'
 import { useThemeProps } from '../Provider/Provider'
 
 // Module augmentation interface for overriding component props' types
 export interface ButtonPropsOverrides {}
 
 export interface ButtonPropsDefaults {
+  children: ReactNode
   /** Button variant color */
   color?: 'primary'
   /** Disables the element, preventing mouse and keyboard events */
   disabled?: boolean
+  /** Icon positioned after button content */
+  endIcon?: string | JSX.Element
   /** Toggles full width element layout */
   fullWidth?: boolean
   /** HTML element href */
   href?: string
   /** Sets the display size */
   size?: 'small' | 'large'
+  /** Icon positioned before button content */
+  startIcon?: string | JSX.Element
+  /** Indicates whether buttons in a disabled state should be wrapped with a span */
+  wrapWhenDisabled?: boolean
   /** Display variant */
   variant?: 'filled' | 'outlined'
 }
@@ -27,7 +36,7 @@ export type ButtonProps = MergePropTypes<ButtonPropsDefaults, ButtonPropsOverrid
 
 // ✨ Nice display type for IntelliSense
 export interface Button {
-  (props: ButtonProps & { ref?: React.ForwardedRef<unknown> }): React.ReactElement | null
+  (props: ButtonProps & { ref?: ForwardedRef<unknown> }): ReactElement | null
   displayName?: string
 }
 
@@ -45,17 +54,38 @@ export interface Button {
 export const Button: Button = forwardRef((props, ref) => {
   const {
     variant = 'filled',
+    children,
+    disabled,
+    startIcon,
+    endIcon,
     color,
     fullWidth,
     size,
+    wrapWhenDisabled = true,
     ...merged
   } = {
     ...useThemeProps('Button'),
     ...props,
   }
 
-  return element({
+  const iconCx = clsx('C9Y-Button-Icon', { [`C9Y-Button-Icon-${size}Size`]: size })
+  const decoratedStartIcon =
+    startIcon === undefined ? null : typeof startIcon === 'string' ? (
+      <Icon id={startIcon} className={iconCx} />
+    ) : (
+      <span className={iconCx}>{startIcon}</span>
+    )
+
+  const decoratedEndIcon =
+    endIcon === undefined ? null : typeof endIcon === 'string' ? (
+      <Icon id={endIcon} className={iconCx} />
+    ) : (
+      <span className={iconCx}>{endIcon}</span>
+    )
+
+  const contents = element({
     ref,
+    disabled,
     // If an href is passed, this instance should render an anchor tag
     as: merged.href ? 'a' : 'button',
     type: merged.href ? undefined : 'button',
@@ -67,7 +97,22 @@ export const Button: Button = forwardRef((props, ref) => {
         'w-full': fullWidth,
       },
     ],
+    children: (
+      <>
+        {decoratedStartIcon}
+        {children}
+        {decoratedEndIcon}
+      </>
+    ),
     ...merged,
   })
+
+  return disabled && wrapWhenDisabled ? (
+    <span className={clsx('C9Y-Button-DisabledWrapper', { 'w-full': fullWidth })}>
+      {contents}
+    </span>
+  ) : (
+    contents
+  )
 })
 Button.displayName = 'Button'
