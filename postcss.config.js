fcdbd05b58e5e8da4943b7085c7e357e017a340d
Dan Hedgecock <8461733+DHedgecock@users.noreply.github.com>
2024-12-23 15:00:18 -0800

    export

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
   plugins: [
     // internal development entry for Componentry plugin, when published this is:
     // require('componentry/postcss')
-    require('./dist/commonjs/plugin-postcss/index'),
+    require('./dist/commonjs/plugin-postcss/index').default,
     require('tailwindcss'),
     require('autoprefixer'),
   ],
